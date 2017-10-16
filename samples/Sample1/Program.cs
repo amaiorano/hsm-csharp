d4ecbdc8b1c59e0831a4ffc5cadcff146de9062d
Antonio Maiorano <amaiorano@gmail.com>
2017-10-15 21:20:41 -0400

    Rework  Sample1 so that StateData is no longer used (or needed)
    This was done by turning the namespace PlayerHsm into an inner class of Player (the owner) so that the states, which are themselves inner classes of PlayerHsm now, can access private data on the owner class. This effetively takes advantage of the fact that inner types have access to an outer type's privates (recursively).

--- samples/Sample1/Program.cs
+++ samples/Sample1/Program.cs
@@ -1,124 +1,132 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace PlayerHsm
+// Would be in a file named Player.cs
+namespace Sample1
+{
+    partial class Player
+    {
+        public partial class PlayerHsm { }
+
+        private Hsm.StateMachine mStateMachine = new Hsm.StateMachine();
+
+        private int Health = 100;
+        private bool StateDataBool = false;
+        private Hsm.StateVar<float> StateVar_Test = new Hsm.StateVar<float>(12.03f);
+
+
+        public void Init()
+        {
+            mStateMachine.Init<PlayerHsm.Root>(this);
+            mStateMachine.TraceLevel = Hsm.TraceLevel.Diagnostic;
+        }
+
+        public void Shutdown()
+        {
+            mStateMachine.Shutdown();
+        }
+
+        public void Update(float aDeltaTime)
+        {
+            mStateMachine.Update(aDeltaTime);
+        }
+    }
+}
+
+// Would be in a file named PlayerHsm.cs
+namespace Sample1
 {
     using Hsm;
-    using PlayerState = Hsm.StateT<Player, StateData>;
+    using PlayerState = Hsm.StateT<Player, Object>;
 
-    class StateData
+    partial class Player
     {
-        public bool StateDataBool = false;
-
-        public StateVar<float> StateVar_Test = new StateVar<float>(12.03f);
-    };
-
-    class Root : PlayerState
-    {
-        public override void OnEnter()
+        public partial class PlayerHsm
         {
-            SetStateVar(Data.StateVar_Test, 1.0f);
-        }
-
-        public override Transition GetTransition()
-        {
-            //return Transition.Sibling<RootSibling), "Hello!", 2);
-            return Transition.Inner<Healthy>();
-        }
-    }
-
-    class Healthy : PlayerState
-    {
-        public override void OnEnter()
-        {
-            var root = GetOuterState<Root>(); // Test being able to grab Root from inner state
-        }
-
-        public override Transition GetTransition()
-        {
-            if (FindInnerState<Driving>() != null)
+            public class Root : PlayerState
             {
-                Console.Out.WriteLine("Healthy state: my inner is Driving!");
+                public override void OnEnter()
+                {
+                    SetStateVar(Owner.StateVar_Test, 1.0f);
+                }
+
+                public override Transition GetTransition()
+                {
+                    //return Transition.Sibling<RootSibling), "Hello!", 2);
+                    return Transition.Inner<Healthy>();
+                }
             }
 
-            return Transition.InnerEntry<Platforming>("Yo!");
-        }
-    }
+            class Healthy : PlayerState
+            {
+                public override void OnEnter()
+                {
+                    var root = GetOuterState<Root>(); // Test being able to grab Root from inner state
+                }
 
-    class Platforming : PlayerState
-    {
-        public override void OnEnter(object[] aArgs)
-        {
-            string s = (string)aArgs[0];
-            SetStateVar(Data.StateVar_Test, 2.0f);
-        }
+                public override Transition GetTransition()
+                {
+                    if (FindInnerState<Driving>() != null)
+                    {
+                        Console.Out.WriteLine("Healthy state: my inner is Driving!");
+                    }
 
-        public override Transition GetTransition()
-        {
-            if (count == 1)
-                return Transition.Sibling<Driving>();
-            ++count;
-            return Transition.None();
-        }
+                    return Transition.InnerEntry<Platforming>("Yo!");
+                }
+            }
 
-        public override void Update(float aDeltaTime)
-        {
-            Console.Out.WriteLine("Player's Health: {0}, StateDataBool: {1}", Owner.Health, Data.StateDataBool);
-            Console.Out.WriteLine("Data.StateVar_Test: {0}", Data.StateVar_Test.Value);
-        }
+            class Platforming : PlayerState
+            {
+                public override void OnEnter(object[] aArgs)
+                {
+                    string s = (string)aArgs[0];
+                    SetStateVar(Owner.StateVar_Test, 2.0f);
+                }
 
-        int count;
-    }
+                public override Transition GetTransition()
+                {
+                    if (count == 1)
+                        return Transition.Sibling<Driving>();
+                    ++count;
+                    return Transition.None();
+                }
 
-    class Driving : PlayerState
-    {
-        public override void Update(float aDeltaTime)
-        {
-            Console.Out.WriteLine("Data.StateVar_Test: {0}", Data.StateVar_Test.Value);
+                public override void Update(float aDeltaTime)
+                {
+                    Console.Out.WriteLine("Player's Health: {0}, StateDataBool: {1}", Owner.Health, Owner.StateDataBool);
+                    Console.Out.WriteLine("Owner.StateVar_Test: {0}", Owner.StateVar_Test.Value);
+                }
 
-            SetStateVar(Data.StateVar_Test, 3.0f);
-        }
-    }
+                int count;
+            }
 
-    class RootSibling : PlayerState
-    {
-        public override void OnEnter(object[] aArgs)
-        {
-            string s = (string)aArgs[0];
-            int i = (int)aArgs[1];
-        }
+            class Driving : PlayerState
+            {
+                public override void Update(float aDeltaTime)
+                {
+                    Console.Out.WriteLine("Owner.StateVar_Test: {0}", Owner.StateVar_Test.Value);
 
-        public override Transition GetTransition()
-        {
-            return Transition.Sibling<Root>();
+                    SetStateVar(Owner.StateVar_Test, 3.0f);
+                }
+            }
+
+            class RootSibling : PlayerState
+            {
+                public override void OnEnter(object[] aArgs)
+                {
+                    string s = (string)aArgs[0];
+                    int i = (int)aArgs[1];
+                }
+
+                public override Transition GetTransition()
+                {
+                    return Transition.Sibling<Root>();
+                }
+            }
         }
     }
 }
 
-class Player
-{
-    private Hsm.StateMachine mStateMachine = new Hsm.StateMachine();
-
-    public int Health = 100;
-
-    public void Init()
-    {
-        mStateMachine.Init<PlayerHsm.Root>(this, new PlayerHsm.StateData());
-        mStateMachine.TraceLevel = Hsm.TraceLevel.Diagnostic;
-    }
-
-    public void Shutdown()
-    {
-        mStateMachine.Shutdown();
-    }
-
-    public void Update(float aDeltaTime)
-    {
-        mStateMachine.Update(aDeltaTime);
-    }
-}
+// Would be in the main program file
 namespace Sample1
 {
     class TestHsm
